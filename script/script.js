e0d99d4f19212387ce6033697e61464ade1b10ab
Luis Guillermo Ramos Díaz <luisitoramos32817@gmail.com>
2025-06-16 22:51:22 -0700

    Se mejoro la responsividad

---
+++ script/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuCheckbox = document.getElementById("menu");
+  const menuIcon = document.querySelector('label[for="menu"] img');
+
+  const menuSrc = "imagenes/menu.png";   // icono hamburguesa
+  const closeSrc = "imagenes/menu-x.png"; // icono X
+
+  function toggleIcon() {
+    if (menuCheckbox.checked) {
+      menuIcon.src = closeSrc;
+    } else {
+      menuIcon.src = menuSrc;
+    }
+  }
+
+  toggleIcon();
+
+  menuCheckbox.addEventListener("change", toggleIcon);
+});
